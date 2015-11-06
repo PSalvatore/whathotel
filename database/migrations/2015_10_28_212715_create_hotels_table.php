e893aae062933d04ffb07ee94334c79ab9930f37
psalvatore <tuc45398@temple.edu>
2015-11-06 11:33:53 -0500

    Adding database migrations

---
+++ database/migrations/2015_10_28_212715_create_hotels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->increments('hotel_id');
+            $table->string('hotel_name', 25)->unique();
+            $table->decimal('nightly_price', 9, 2);
+            $table->string('valet', 1);
+            $table->string('image_name', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hotels');
+    }
+}
---
+++ database/migrations/2015_10_28_220720_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('reservation_id');
+            $table->integer('nights_qty', false)->length(5);
+            $table->date('start_date');
+            $table->integer('room_number', false)->length(6);
+            $table->string('suite', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reservations');
+    }
+}
