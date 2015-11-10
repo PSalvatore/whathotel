45a269cdb9445aa48601fcd47c846b88a626711c
psalvatore <tuc45398@temple.edu>
2015-11-10 17:32:01 -0500

    Admin only update and delete

--- resources/views/hotels/index.blade.php
+++ resources/views/hotels/index.blade.php
@@ -57,62 +57,73 @@
                 </tr>
             </thead>
             <tbody>
-          @foreach($hotels as $hotel)
-
-                <tr>
-                    <td><a href={{ url('hotels/delete/'.$hotel->hotel_id) }}><img src="{{ URL::to('/') }}/icons/delete.png"></a>
-                        {{$hotel->hotel_id}}
-                    </td>
-                    <td class="name_col">
-                        {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/hotel_name')) !!}
-                        <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
-                        <div class="text" id="text1">{{$hotel->hotel_name}}</div>
-                        <div class="input-group edit" id="edit1">
-                            <input type="text" name="hotel_name" id="hotel_name" class="form-control" placeholder="{{$hotel->hotel_name}}">
-                            <span class="input-group-btn">
-                                <button type="submit" class="btn btn-default">Edit</button>
-                            </span>
-                        </div>
-                        {!! Form::close() !!}
-                    </td>
-                    <td class="price_col">
-                        {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/nightly_price')) !!}
-                        <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
-                        <div class="text" id="text2">{{$hotel->nightly_price}}</div>
-                        <div class="input-group edit" id="edit2">
-                            <input type="text" name="nightly_price" id="nightly_price" class="form-control" placeholder="{{$hotel->nightly_price}}" >
-                            <span class="input-group-btn">
-                                <button type="submit" class="btn btn-default">Edit</button>
-                            </span>
-                        </div>
-                        {!! Form::close() !!}
-                    </td>
-                    <td class="valet_col">
-                        {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/valet')) !!}
-                        <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
-                        <div class="text" id="text3">{{$hotel->valet}}</div>
-                        <div class="input-group edit" id="edit3">
-                            <input type="text" name="valet" id="valet" class="form-control" placeholder="{{$hotel->valet}}">
-                            <span class="input-group-btn">
-                                <button type="submit" class="btn btn-default">Edit</button>
-                            </span>
-                        </div>
-                        {!! Form::close() !!}
-                    </td>
-                    <td class="img_col">
-                        {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/image_name')) !!}
-                        <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
-                        <div class="text" id="text4">{{$hotel->image_name}}</div>
-                        <div class="input-group edit" id="edit4">
-                            <input type="text" name="image_name" id="imagename" class="form-control" placeholder="{{$hotel->image_name}}">
-                            <span class="input-group-btn">
-                                <button type="submit" class="btn btn-default">Edit</button>
-                            </span>
-                        </div>
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
-          @endforeach
+            @if(Auth::check() && Auth::user()->username === 'admin')
+                @foreach($hotels as $hotel)
+                    <tr>
+                        <td><a href={{ url('hotels/delete/'.$hotel->hotel_id) }}><img src="{{ URL::to('/') }}/icons/delete.png"></a>
+                            {{$hotel->hotel_id}}
+                        </td>
+                        <td class="name_col">
+                            {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/hotel_name')) !!}
+                            <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
+                            <div class="text" id="text1">{{$hotel->hotel_name}}</div>
+                            <div class="input-group edit" id="edit1">
+                                <input type="text" name="hotel_name" id="hotel_name" class="form-control" placeholder="{{$hotel->hotel_name}}">
+                                <span class="input-group-btn">
+                                    <button type="submit" class="btn btn-default">Edit</button>
+                                </span>
+                            </div>
+                            {!! Form::close() !!}
+                        </td>
+                        <td class="price_col">
+                            {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/nightly_price')) !!}
+                            <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
+                            <div class="text" id="text2">{{$hotel->nightly_price}}</div>
+                            <div class="input-group edit" id="edit2">
+                                <input type="text" name="nightly_price" id="nightly_price" class="form-control" placeholder="{{$hotel->nightly_price}}" >
+                                <span class="input-group-btn">
+                                    <button type="submit" class="btn btn-default">Edit</button>
+                                </span>
+                            </div>
+                            {!! Form::close() !!}
+                        </td>
+                        <td class="valet_col">
+                            {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/valet')) !!}
+                            <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
+                            <div class="text" id="text3">{{$hotel->valet}}</div>
+                            <div class="input-group edit" id="edit3">
+                                <input type="text" name="valet" id="valet" class="form-control" placeholder="{{$hotel->valet}}">
+                                <span class="input-group-btn">
+                                    <button type="submit" class="btn btn-default">Edit</button>
+                                </span>
+                            </div>
+                            {!! Form::close() !!}
+                        </td>
+                        <td class="img_col">
+                            {!! Form::open(array('url' => 'hotels/update/'.$hotel->hotel_id.'/image_name')) !!}
+                            <img class="icon" id="icon{{$hotel->hotel_id}}" src="{{ URL::to('/') }}/icons/update.png">
+                            <div class="text" id="text4">{{$hotel->image_name}}</div>
+                            <div class="input-group edit" id="edit4">
+                                <input type="text" name="image_name" id="image_name" class="form-control" placeholder="{{$hotel->image_name}}">
+                                <span class="input-group-btn">
+                                    <button type="submit" class="btn btn-default">Edit</button>
+                                </span>
+                            </div>
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                @foreach($hotels as $hotel)
+                    <tr>
+                        <td>{{$hotel->hotel_id}}</td>
+                        <td>{{$hotel->hotel_name}}</td>
+                        <td>{{$hotel->nightly_price}}</td>
+                        <td>{{$hotel->valet}}</td>
+                        <td>{{$hotel->image_name}}</td>
+                    </tr>
+                @endforeach
+            @endif
           </tbody>
         </table>
     </div>
