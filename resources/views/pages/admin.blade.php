55590e22e2263fb0225bd7c3bb5916d3a52cdf59
psalvatore <tuc45398@temple.edu>
2015-11-10 21:56:29 -0500

    minor alteration

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.default')
 @section('content')
-        <h1>Welcome to the Admin page <b>{{$username}}</b></h1>
+        <h1>Welcome to the Admin page, <b>{{$username}}</b></h1>
 @stop
